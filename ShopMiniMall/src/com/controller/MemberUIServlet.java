426abb9f52b01cd589a1060e15250c8f27d7fe9a
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2023-08-22 23:12:16 +0900

    쇼핑몰 구현 프로젝트

---
+++ ShopMiniMall/src/com/controller/MemberUIServlet.java
@@ -0,0 +1,20 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/MemberUIServlet")
+public class MemberUIServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//포워드
+		request.getRequestDispatcher("memberForm.jsp").forward(request, response);
+	}
+
+}
